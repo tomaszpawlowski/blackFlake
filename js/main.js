7eb598134caee5c5fdc1e9ea537df0f40093befe
Tomasz Pawłowski <pawlowskitomasz91@gmail.com>
2023-10-11 19:50:28 +0200

    Main functionality

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let slideIndex = 0;
+slideShow();
+
+function slideShow() {
+  let i;
+  let slides = document.querySelectorAll('ul.slideShowMaster li');
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.opacity = "0";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.opacity = "1";
+  setTimeout(slideShow, 2000); // Change image every 2 seconds
+}
